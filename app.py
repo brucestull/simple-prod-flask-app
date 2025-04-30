905b1e3a6175c0dc9a68b0f7fa7becf09f3925af
Flynnt Knapp <bruce.stull@gmail.com>
2025-04-30 18:26:52 -0400

    Add simple flask app app.py.

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, this is a production-ready test Flask app!"
+
+if __name__ == "__main__":
+  app.run()
